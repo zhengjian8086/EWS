6f2b5e54f9b5a6e2249ec818276690cb9c2a92bb
zhengjian8086 <z731000532@hotmail.com>
2020-08-14 17:49:04 +0800

    Update celery demo use rabbitmq borker

--- TestScript/celery_tmp/tasks.py
+++ TestScript/celery_tmp/tasks.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 app = Celery('demo',
-             broker='redis://@127.0.0.1:6379/1',
+             broker='amqp://guest@127.0.0.1//',
              backend='redis://@127.0.0.1:6379/2',
              )
 
